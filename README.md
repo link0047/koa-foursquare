afbe74e9fa3729bbd5675ca1b2b527094ff25cbe
link0047 <link0047@gmail.com>
2015-08-17 11:48:08 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+
+# Koa foursquare
+
+[![NPM version][npm-image]][npm-url]
+[![Build status][travis-image]][travis-url]
+[![Test coverage][coveralls-image]][coveralls-url]
+[![Dependency Status][david-image]][david-url]
+[![License][license-image]][license-url]
+[![Downloads][downloads-image]][downloads-url]
+[![Gittip][gittip-image]][gittip-url]
+
+Similar to [foursquarevenues][foursquarevenues],
+except it uses [koa-request][koa-request] instead of [request][request] to
+access [[foursquares venue API](https://developer.foursquare.com/overview/venues)]
---
+++ index.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var request     = require('koa-request');
+var querystring = require('querystring');
+
+module.exports = function (client_id, client_secret) {
+  var baseUrl = 'https://api.foursquare.com/v2/venues/';
+  var credentials = querystring.stringify({
+    v: '20140806',
+    client_id: client_id,
+    client_secret: client_secret
+  });
+
+  return {
+    getCategories: function *(params) {
+      var url = baseUrl + 'categories?' + credentials;
+      var res = yield request(url);
+      return JSON.parse(res.body);
+    },
+    getVenues: function *(params) {
+      var url = baseUrl + 'search?' + querystring.stringify(params) + '&' + credentials;
+      var res = yield request(url);
+      return JSON.parse(res.body);
+    },
+    exploreVenues: function *(params) {
+      var url = baseUrl + 'explore?' + querystring.stringify(params) + '&' + credentials;
+      var res = yield request(url);
+      return JSON.parse(res.body);
+    },
+    getVenue: function *(params) {
+      var url = baseUrl + params.venue_id + '&' + credentials;
+      var res = yield request(url);
+      return JSON.parse(res.body);
+    }
+  }
+}
